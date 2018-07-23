82642684a8f493f81f1de5c88df3f2eaa5c1eccd
Alex <96dalex@gmail.com>
2018-07-23 12:40:01 -0600

    give messages from the server a uuid

--- backend/lib/backend/message.ex
+++ backend/lib/backend/message.ex
@@ -1,6 +1,6 @@
 defmodule Backend.Message do
   use Protobuf, from: Path.wildcard(Path.expand("../../../schema/**/*.proto", __DIR__))
-  alias Backend.Message.{PlayerEntity, CreationEvent, StatusUpdate, ServerMessage}
+  alias Backend.Message.{PlayerEntity, CreationEvent, StatusUpdate, ServerMessage, Uuid}
 
   def temp_gen_server_message_1() do
     entity = PlayerEntity.new(%{size: 60, direction: :STOP})
@@ -10,8 +10,19 @@ defmodule Backend.Message do
       entity: {:player, entity}
     })
     status_update = StatusUpdate.new(%{payload: {:creation_event, event}})
-    server_message = ServerMessage.new(%{payload: {:status_update, status_update}})
+    server_message = ServerMessage.new(%{id: generate_uuid(), payload: {:status_update, status_update}})
     ServerMessage.encode(server_message)
+  end
+
+  def generate_uuid() do
+    [part2, part1] = UUID.uuid4()
+      |> UUID.string_to_binary!
+      |> :binary.bin_to_list
+      |> Enum.reverse
+      |> Enum.chunk(8)
+      |> Enum.map(&Integer.undigits(&1, 256))
+
+    Uuid.new(%{data_1: part1, data_2: part2})
   end
 
 end
--- backend/lib/backend_web/channels/game_channel.ex
+++ backend/lib/backend_web/channels/game_channel.ex
@@ -25,7 +25,7 @@ defmodule BackendWeb.GameChannel do
   end
 
   def handle_in("temp_gen_server_message_1", _data, socket) do
-    msg = <<0, 0>> <> Backend.Message.temp_gen_server_message_1
+    msg = Backend.Message.temp_gen_server_message_1
     push(socket, "temp_gen_server_message_1_res", %{msg: msg})
     {:noreply, socket}
   end
