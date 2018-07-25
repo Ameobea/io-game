ab327c7d3b3477973d72a90bd3764d3cec14122d
Alex <96dalex@gmail.com>
2018-07-24 20:07:40 -0600

    send binary to the frontend correctly

--- backend/lib/backend_web/channels/game_channel.ex
+++ backend/lib/backend_web/channels/game_channel.ex
@@ -26,7 +26,7 @@ defmodule BackendWeb.GameChannel do
 
   def handle_in("temp_gen_server_message_1", _data, socket) do
     msg = Backend.Message.temp_gen_server_message_1
-    push(socket, "temp_gen_server_message_1_res", %{msg: msg})
+    push(socket, "temp_gen_server_message_1_res", %{msg: :binary.bin_to_list(msg)})
     {:noreply, socket}
   end
 end
